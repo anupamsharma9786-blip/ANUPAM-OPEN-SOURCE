6ce7f6eaa4c09590cf66f3c0e4e9e81773471524
anupam <anupamsharma9786@gmail.com>
2025-11-13 22:02:05 +0530

    js task 1

---
+++ JS T1/app.js
@@ -0,0 +1,16 @@
+let h1  = document.querySelector('h1');
+let inc = document.querySelector('#inc');
+let dec = document.querySelector('#dec');
+let count = 0;
+
+inc.addEventListener('click',function (){
+    count++;
+    h1.innerHTML = count;
+
+})
+
+dec.addEventListener('click',function (){
+    count--;
+    h1.innerHTML = count;
+
+})
